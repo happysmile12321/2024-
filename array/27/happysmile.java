62367d5ee625ef0a460cbc39e90f5bf876b16669
happysmile12321 <41978045+happysmile12321@users.noreply.github.com>
2024-02-23 15:19:12 +0800

    Update happysmile.java

--- array/27/happysmile.java
+++ array/27/happysmile.java
@@ -1,1 +1,12 @@
-
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int leftHand = -1, rightHand = 0;
+        for(;rightHand<nums.length;rightHand++){
+            if(nums[rightHand]!=val){
+                leftHand++;
+                nums[leftHand] = nums[rightHand];
+            }
+        }
+        return leftHand+1;
+    }
+}
