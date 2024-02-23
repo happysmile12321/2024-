9a3dd9b52bc14752c1d2f8e94f770fa1f08118c9
OvvO <1553223442@qq.com>
2024-02-23 22:06:52 +0800

    fix(704):704题代码提交

---
+++ array/704/OvvO.java
@@ -0,0 +1,28 @@
+/**
+ * 给定一个 n 个元素有序的（升序）整型数组 nums 和一个目标值 target  ，
+ * 写一个函数搜索 nums 中的 target，如果目标值存在返回下标，否则返回 -1。
+ *
+ * @Author: OvvO
+ * @Description: TODO
+ * @Date: Created in 22:01 2024/2/23
+ * @Modified By:
+ */
+public class OvvO {
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = (right - left) / 2 + left;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return -1;
+    }
+}
