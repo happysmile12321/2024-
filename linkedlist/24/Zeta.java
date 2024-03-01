c263550c46cca8d383dbe6c0a4a3d947951e500d
juefei yan <juefeiyan@juefeis-MacBook-Air.local>
2024-02-29 23:35:53 -0800

    Zeta 24

---
+++ linkedlist/24/Zeta.java
@@ -0,0 +1,20 @@
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+
+        // If the list has no node or has only one node left.
+        if ((head == null) || (head.next == null)) {
+            return head;
+        }
+
+        // Nodes to be swapped
+        ListNode firstNode = head;
+        ListNode secondNode = head.next;
+
+        // Swapping
+        firstNode.next  = swapPairs(secondNode.next);
+        secondNode.next = firstNode;
+
+        // Now the head is the second node
+        return secondNode;
+    }
+}
