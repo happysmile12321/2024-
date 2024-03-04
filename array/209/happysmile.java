8e9d44a38b8b43fa0b08ba515d8cc8bcf658c97f
happysmile12321 <41978045+happysmile12321@users.noreply.github.com>
2024-03-04 23:04:43 +0800

    Update happysmile.java

--- array/209/happysmile.java
+++ array/209/happysmile.java
@@ -1,1 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=209 lang=java
+ *
+ * [209] 长度最小的子数组
+ */
 
+// @lc code=start
+class Solution {
+    public int minSubArrayLen(int target, int[] nums) {
+        int i = 0, j = 0;
+        int sum = 0;
+        int res = Integer.MAX_VALUE;
+        for (; j < nums.length; j++) {
+            sum += nums[j];
+            while (i <= j && sum >= target) {
+                int subLen = (j - i + 1);
+                res = res < subLen ? res : subLen;
+                sum -= nums[i++];
+            }
+        }
+        return res == Integer.MAX_VALUE ? 0 : res;
+    }
+}
+// @lc code=end
